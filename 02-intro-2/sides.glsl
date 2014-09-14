7487027d2849dfbc2d6131168e865b6d724b3267
yoshuawuyts <i@yoshuawuyts.com>
2014-09-14 15:16:29 +0200

    2

--- 02-intro-2/sides.glsl
+++ 02-intro-2/sides.glsl
@@ -1,12 +1,12 @@
 void sideLengths(
-  highp float hypotenuse, 
-  highp float angleInDegrees, 
-  out highp float opposite, 
+  float hypotenuse,
+  float angleInDegrees,
+  out highp float opposite,
   out highp float adjacent) {
 
-
-  //TODO: Calculate side lengths here
-
+  float theta = radians(angleInDegrees);
+  opposite = hypotenuse * sin(theta);
+  adjacent = hypotenuse * cos(theta);
 }
 
 //Do not change this line
